fd52d10168653ea3b631948e247af9cac44819e1
jyothi.N <jyoozedu22@gmail.com>
2025-11-19 07:54:25 +0530

    Update program1a.c

--- program1a.c
+++ program1a.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int number; // Declare an integer variable
+    int number; 
 
-    printf("Enter an integer: "); // Prompt the user for input
-    scanf("%d", &number); // Read an integer from the user and store it in 'number'
+    printf("Enter an integer: "); 
+    scanf("%d", &number); 
 
-    printf("You entered: %d\n", number); // Print the entered number
+    printf("You entered: %d\n", number); 
 
     return 0;
 }
+
