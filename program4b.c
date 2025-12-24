b8767a12335f1d7280ea4c38766626a8f506a4f7
jyothi.N <jyoozedu22@gmail.com>
2025-12-24 09:12:22 +0530

    Update program4b.c

--- program4b.c
+++ program4b.c
@@ -3,20 +3,19 @@
 int main() {
     int age;
 
-    // Prompt the user to enter their age
     printf("Enter your age: ");
 
-    // Read the age entered by the user
+    
     scanf("%d", &age);
 
-    // Check if the age meets the voting eligibility criteria (18 or older)
     if (age >= 18) {
         printf("You are eligible to vote.\n");
     } else {
         printf("You are not eligible to vote.\n");
-        // Optionally, inform the user how many years until they are eligible
+     
         printf("You will be eligible to vote in %d year(s).\n", 18 - age);
     }
 
-    return 0; // Indicate successful program execution
+    return 0; 
+
 }
